64950994f45a4f1574400b3c5231677aef1ed65f
Nugie Jaya Nugraha <Nugie Jaya Nugraha>
2022-08-29 23:19:07 +0700

    add update profile

--- controllers/user.js
+++ controllers/user.js
@@ -166,10 +166,43 @@ const getUserByName = async (req, res) => {
 }
 
 
+// update profile
+const updateUser = async (req, res) => {
+
+  const email = req.body.email;
+  const city = req.body.city;
+
+  const emailAlreadyRegistered = await user_game.findOne({
+    where: { email: email }
+  });
+
+  if (emailAlreadyRegistered) {
+    return res.status(401).json({
+      message: "Email sudah terdaftar"
+    });
+  }
+
+  await user_game.update({
+    email: email,
+    city: city
+  }, {
+    where: { username: req.params.username }
+  });
+
+  return res.status(200).json({
+    message: "Update berhasil",
+    data: {
+      email,
+      city 
+    }
+  });
+}
+
 
 module.exports = {
   login,
   register,
   showPlayers,
-  getUserByName
+  getUserByName,
+  updateUser
 };
