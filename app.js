4e56f19b814f20f9788574b01bf8ce5fd07e4a5e
Nugie Jaya Nugraha <Nugie Jaya Nugraha>
2022-08-31 22:51:50 +0700

    add cloudinary

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
+
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4000;
 const cors = require("cors");
+const multer = require('multer');
 
 // import router
 const router = require("./routers");
@@ -10,6 +13,7 @@ const router = require("./routers");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(multer().any());
 
 // Routes
 app.use("/", router);
